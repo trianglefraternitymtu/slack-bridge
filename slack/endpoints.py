18b50af56adf770079f067cb8b6cbf8bfffafb91
Ian Wakely <raveious.irw@gmail.com>
2017-05-05 13:24:29 -0400

    The docs are kind of inconsistant...

--- slack/endpoints.py
+++ slack/endpoints.py
@@ -60,11 +60,12 @@ def action(request):
 @require_POST
 def event(request):
     logger.info('Event Push')
-    logger.debug(request.POST)
+    body = request.POST.body
+    logger.debug(body)
 
-    token = request.POST.get('token')
-    event_type = request.POST.get('type')
-    challenge = request.POST.get('challenge', None)
+    token = body.get('token')
+    event_type = body.get('type')
+    challenge = body.get('challenge', None)
 
     if not verified_token(token):
         logger.warning("Token verification failed. ({})".format(token))

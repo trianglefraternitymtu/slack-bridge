c553623ff257df50583af6c6077da9a1d81868db
Ian Wakely <raveious.irw@gmail.com>
2017-05-06 01:01:12 -0400

    Removing dotenv because Heroku does it for you.

--- manage.py
+++ manage.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
-import dotenv
 
 if __name__ == "__main__":
-    dotenv.read_dotenv()
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
     from django.core.management import execute_from_command_line

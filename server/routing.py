ecec78839ad8b508ddda52b9d1b668b4b19e15a4
Ian Wakely <raveious.irw@gmail.com>
2017-05-07 17:09:58 -0400

    removing unneeded routs and unused code.

--- server/routing.py
+++ server/routing.py
@@ -3,5 +3,4 @@ import slack.consumers
 
 channel_routing = [
     route('background-slack-event', slack.consumers.event),
-    route('background-slack-share-msg', slack.consumers.share_msg),
 ]
--- slack/consumers.py
+++ slack/consumers.py
@@ -16,21 +16,18 @@ def event(message):
     logger.debug(payload)
 
     event = payload['event']
-    args = {}
 
     local_team = get_object_or_404(Team, team_id=payload['team_id'])
     local_team_interface = Slacker(local_team.app_access_token)
 
-    if payload.get('api_app_id') == os.environ.get('SLACK_APP_ID'):
-        logger.info("Ignoring stuff sent from myself...")
-    elif event['type'] != "message":
+    if event['type'] != "message":
         logger.warning('Not sure what "{}" event is...'.format(event['type']))
     elif event.get('subtype') == 'bot_message':
         logger.info("Ignoring stuff by other bots...")
     elif event.get("subtype") == "channel_join":
         logger.info("Bot was added to channel {} on team {}".format(event['channel'], local_team.team_id))
         SharedChannel.objects.get_or_create(channel_id=event['channel'],
-        local_team=local_team)
+                                            local_team=local_team)
     elif event.get('user') == 'USLACKBOT':
         if "removed from" in event['text']:
             logger.info("Bot was removed from channel {} on team {}".format(event['channel'], local_team.team_id))
@@ -52,13 +49,3 @@ def event(message):
                                     icon_url=user_info['profile']['image_192'],
                                     as_user=False
                                     )
-        # Channel("background-slack-share-msg").send({
-        #                                                 'event':event,
-        #                                                 'target':target.id,
-        #                                                 'user_info':user_info
-        #                                             })
-
-def share_msg(message):
-    logger.info("Executing slack responses")
-    data = message.content
-    logger.debug(data)

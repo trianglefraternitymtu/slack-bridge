74021aeacd506f021934beeab7c6449c15b476fb
Ian Wakely <raveious.irw@gmail.com>
2017-05-06 20:31:07 -0400

    Seperating the function determination.

--- slack/consumers.py
+++ slack/consumers.py
@@ -15,6 +15,7 @@ def event(message):
     logger.debug(payload)
 
     event = payload['event']
+    args = {}
 
     local_team = get_object_or_404(Team, team_id=payload['team_id'])
     local_team_interface = Slacker(local_team.app_access_token)
@@ -24,23 +25,29 @@ def event(message):
         return
     elif event['type'] == "message":
         user_info = local_team_interface.users.info(event['user']).body['user']
-        for target in Team.objects.all():#.exclude(team_id=payload['team_id']):
-            slack = Slacker(target.app_access_token)
-            logger.debug(target)
 
-            func = slack.chat.post_message
+        args = {
+            "text":event.get('text'),
+            "attachments":event.get('attachments'),
+            "channel":event['channel'],
+            "username":user_info['profile']['real_name'],
+            "icon_url":user_info['profile']['image_192'],
+            "as_user":False
+        }
 
-            args = {
-                "text":event.get('text'),
-                "attachments":event.get('attachments'),
-                "channel":event['channel'],
-                "username":user_info['profile']['real_name'],
-                "icon_url":user_info['profile']['image_192'],
-                "as_user":False
-            }
-
-            Channel("background-slack-share-msg").send({'func':func, 'args':args})
+    for target in Team.objects.all():#.exclude(team_id=payload['team_id']):
+        Channel("background-slack-share-msg").send({
+                                            'func':event['type'],
+                                            'args':args,
+                                            'api_token':target.app_access_token
+                                        })
 
 def share_msg(message):
-    payload = message.content
-    payload['func'](**payload['args'])
+    slack = Slacker(message.content['api_token'])
+    func = None
+
+    if message.content['func'] == "message":
+        func = slack.chat.post_message
+
+    if not func:
+        func(**message.content['args'])

35d05fd3377cab2ecb4d7a1e5934be6afb8256ac
Ian Wakely <raveious.irw@gmail.com>
2017-05-30 20:21:38 -0500

    Splitting up updating and posting

--- slack/consumers.py
+++ slack/consumers.py
@@ -19,14 +19,17 @@ def event(message):
     payload = message.content
     event = payload['event']
     subtype = event.get('subtype')
+    subsubtype = (event.get('message', {}).get('subtype') or event.get('previous_message', {}).get('subtype'))
 
     local_team = get_object_or_404(Team, team_id=payload['team_id'])
     local_team_interface = Slacker(local_team.app_access_token)
 
-    if event['type'] != "message":
+    sa_text = clear_tags(local_team_interface, event.get('text', ''))
+
+    if event['type'] not in ["message"]:
         logger.warning('Not sure what "{}" event is...'.format(event['type']))
 
-    elif subtype == 'bot_message':
+    elif subtype == 'bot_message' or subsubtype == 'bot_message':
         logger.info("Ignoring stuff by other bots...")
 
     elif subtype == "channel_join" and event.get("user") == local_team.bot_id:
@@ -45,21 +48,50 @@ def event(message):
 
         else:
             logger.info("Ignoring slackbot updates")
-    else:
-        if subtype in ('message_changed', 'message_deleted'):
-            user_info = None
-        else:
-            user_info = local_team_interface.users.info(event['user']).body['user']
 
-        sa_text = clear_tags(local_team_interface, event.get('text', ''))
-
-        for target in SharedChannel.objects.exclude(channel_id=event['channel'], local_team=local_team):
+    elif subtype in ('message_changed', 'message_deleted'):
+        for target in _otherChannels(event['channel'], local_team):
             if target.local_team.team_id != local_team.team_id:
-                event['text'] = sa_text
+                payload['event']['text'] = sa_text
             Channel("background-slack-update").send({"payload":payload,
-                                                     "user":user_info,
                                                      "channel_id":target.channel_id,
                                                      "team_id":target.local_team.team_id})
+
+    else:
+        user_info = local_team_interface.users.info(event['user']).body['user']
+
+        for target in _otherChannels(event['channel'], local_team):
+            if target.local_team.team_id != local_team.team_id:
+                payload['event']['text'] = sa_text
+            Channel("background-slack-post").send({"payload":payload,
+                                                   "user":user_info,
+                                                   "channel_id":target.channel_id,
+                                                   "team_id":target.local_team.team_id})
+
+def _otherChannels(ch_id, team):
+    return SharedChannel.objects.exclude(channel_id=ch_id, local_team=team)
+
+def post(message):
+    payload = message.content['payload']
+    event = payload['event']
+    user = message.content['user']
+    subtype = event.get('subtype')
+    subsubtype = (event.get('message', {}).get('subtype') or event.get('previous_message', {}).get('subtype'))
+
+    logger.debug("Posting message ({}<{}>).".format(subtype, subsubtype))
+
+    team = get_object_or_404(Team, team_id=message.content['team_id'])
+    team_interface = Slacker(team.app_access_token)
+
+    if subtype in ["channel_join", "channel_leave"]:
+        event['text'] = '_{}_'.format(event['text'])
+
+    team_interface.chat.post_message(text=event['text'],
+                            attachments=event.get('attachments'),
+                            channel=message.content['channel_id'],
+                            username=(user['profile']['real_name'] or user['profile']['name']),
+                            icon_url=user['profile']['image_192'],
+                            as_user=False)
 
 def update(message):
     payload = message.content['payload']
@@ -73,10 +105,7 @@ def update(message):
     team = get_object_or_404(Team, team_id=message.content['team_id'])
     team_interface = Slacker(team.app_access_token)
 
-    if subsubtype == 'bot_message':
-        logger.info("Ignoring stuff by other bots...")
-
-    elif subtype == "message_changed":
+    if subtype == "message_changed":
         msgs = team_interface.channels.history(message.content['channel_id'],
                                                count=100).body['messages']
 
@@ -90,7 +119,7 @@ def update(message):
                                            as_user=False,
                                            ts=msg['ts'],
                                            text=sa_text,
-                                           attachments=event.get('attachments'))
+                                           attachments=event['message'].get('attachments'))
                 break
 
     elif subtype == "message_deleted":
@@ -105,13 +134,3 @@ def update(message):
                                            ts=msg['ts'],
                                            as_user=False)
                 break
-    else:
-        if event.get('subtype') in ["channel_join", "channel_leave"]:
-            event['text'] = '_{}_'.format(event['text'])
-
-        team_interface.chat.post_message(text=event['text'],
-                                attachments=event.get('attachments'),
-                                channel=message.content['channel_id'],
-                                username=(user['profile']['real_name'] or user['profile']['name']),
-                                icon_url=user['profile']['image_192'],
-                                as_user=False)
